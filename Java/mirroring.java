35d43db3f1910d1dee9d49f6e9b936684eda30ea
SAMATHANAPRABU SHANMUGAM <76841467+samathanaprabu@users.noreply.github.com>
2024-12-04 23:45:30 +0300

    Create mirroring.java
    create file

---
+++ Java/mirroring.java
@@ -0,0 +1,13 @@
+```  
+class Main {
+    public static void main(String[] args) {
+        int n=6;
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+               System.out.print(j);
+           }
+           System.out.println();
+       }
+    }
+}
+```
