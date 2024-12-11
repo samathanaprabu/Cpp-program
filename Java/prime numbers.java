823e1d0b85be22c53cf3535863847ca67a12c21d
SAMATHANAPRABU SHANMUGAM <76841467+samathanaprabu@users.noreply.github.com>
2024-12-11 20:53:34 +0300

    Create prime numbers.java

---
+++ Java/prime numbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
